833cc2846a0cc022e34d0428ad2376ac9c08f891
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-05-23 17:50:13 -0300

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    public class Progam
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci(3);
+         }
+
+        public static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return 1;
+
+            var result = Fibonacci(n - 1) + Fibonacci(n - 2);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
